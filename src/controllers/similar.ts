3845fed21c4a2a1a99286bb638dc4a64944a0baf
Mantton <mantton.official@gmail.com>
2022-09-06 23:34:45 -0400

    deps: add nodemone

--- src/controllers/similar.ts
+++ src/controllers/similar.ts
@@ -5,7 +5,6 @@ import { getSimilarTitles } from "../services/similar";
 export const handleGetSimilar: RequestHandler = async (req, res, next) => {
   const sourceId = req.params.source as string;
   const contentId = req.params.content as string;
-
   const page = parseInt(req.query.page as string) || 1;
   try {
     const results = await getSimilarTitles(
@@ -14,7 +13,6 @@ export const handleGetSimilar: RequestHandler = async (req, res, next) => {
       Math.max(page, 1)
     );
     setToCache({ sourceId, contentId }, results);
-
     res.send(results);
   } catch (err) {
     next(err);
